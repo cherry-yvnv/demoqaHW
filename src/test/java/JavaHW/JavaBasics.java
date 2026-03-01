1ef451870233a525bf142c63e3ebaddd2d6fda3f
cherry-yvnv <yvnvgg@gmail.com>
2026-03-01 18:49:02 +0300

    added Registration Test

---
+++ src/test/java/JavaHW/JavaBasics.java
@@ -0,0 +1,33 @@
+package JavaHW;
+
+public class JavaBasics {
+    public static void main(String[] args) {
+        int A = 66;
+        int B = 77;
+        double C = 22.2;
+        double D = Double.MAX_VALUE;
+
+
+        System.out.println(A + B);
+        System.out.println(B - A);
+        System.out.println(A * B);
+        System.out.println(B / A);
+
+        System.out.println(A * C);
+        System.out.println(C * C);
+        System.out.println(B * D);
+
+        System.out.println(A > B);
+        System.out.println(A < B);
+        System.out.println(A != B);
+        System.out.println(A == B);
+        System.out.println(C >= B);
+        System.out.println(C == C);
+        System.out.println(C <= B);
+
+        System.out.println(D * D);
+        System.out.println(D / D);
+        System.out.println(A + D);
+    }
+}
+
