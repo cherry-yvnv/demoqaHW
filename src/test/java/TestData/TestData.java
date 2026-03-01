c0a8e144f14fb54dc688ddca1c87444fe5effb0b
cherry-yvnv <yvnvgg@gmail.com>
2026-03-01 20:12:21 +0300

    added registration test and data

---
+++ src/test/java/TestData/TestData.java
@@ -0,0 +1,18 @@
+package TestData;
+
+public class TestData {
+    public static String firstName = "Britney";
+    public static String lastName = "Spears";
+    public static String userEmail = "britney@spears.com";
+    public static String sex = "Female";
+    public static String userNumber = "8987654321";
+    public static String month = "March";
+    public static String year = "1994";
+    public static String day = "04";
+    public static String subject = "Maths";
+    public static String hobby = "Music";
+    public static String picture = "img.png";
+    public static String currentAddress = "San-Andreas";
+    public static String state = "NCR";
+    public static String city = "Gurgaon";
+}
---
+++ src/test/java/demoqaTests/StudentRegistrationFormWithDataTest.java
@@ -0,0 +1,52 @@
+package demoqaTests;
+
+import TestData.TestBase;
+import org.junit.jupiter.api.Test;
+
+import static TestData.TestData.*;
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class StudentRegistrationFormWithDataTest extends TestBase {
+
+    @Test
+    void successfulRegistrationFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        $("#genterWrapper").$(byText(sex)).click();
+        $("#userNumber").setValue(userNumber);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").$(byText(month)).click();
+        $(".react-datepicker__year-select").$(byText(year)).click();
+        $(".react-datepicker__day.react-datepicker__day--0" + day).click();
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $("#hobbies-checkbox-3").click();
+        $("#uploadPicture").uploadFromClasspath(picture);
+        $("#currentAddress").setValue(currentAddress);
+        $("#state").click();
+        $(byText(state)).click();
+        $("#city").click();
+        $(byText(city)).click();
+        $("#submit").click();
+
+        $(".modal-open").should(appear);
+        $(".table-responsive") .shouldHave(text(firstName));
+        $(".table-responsive") .shouldHave(text(lastName));
+        $(".table-responsive").shouldHave(text(userEmail));
+        $(".table-responsive").shouldHave(text(sex));
+        $(".table-responsive").shouldHave(text(userNumber));
+        $(".table-responsive").shouldHave(text(day + " " + month + "," + year));
+        $(".table-responsive").shouldHave(text(subject));
+        $(".table-responsive").shouldHave(text(hobby));
+        $(".table-responsive").shouldHave(text(currentAddress));
+        $(".table-responsive").shouldHave(text(picture));
+        $(".table-responsive").shouldHave(text(state + " " + city));
+        $("#closeLargeModal").click();
+
+    }
+}
