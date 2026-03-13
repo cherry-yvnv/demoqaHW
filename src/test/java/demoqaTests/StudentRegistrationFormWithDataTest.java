f84857231257beee58b206826bc052d48a7e9ee5
cherry-yvnv <yvnvgg@gmail.com>
2026-03-13 23:59:08 +0300

    added PageObject for Registration Form

--- src/test/java/demoqaTests/StudentRegistrationFormWithDataTest.java
+++ src/test/java/demoqaTests/StudentRegistrationFormWithDataTest.java
@@ -1,55 +1,48 @@
 package demoqaTests;
 
+import pages.StudentRegistrationFormPage;
 import test_data.TestBase;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.*;
+
 import static test_data.TestData.*;
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
+
 
 public class StudentRegistrationFormWithDataTest extends TestBase {
-
+    StudentRegistrationFormPage studentRegistrationFormPage = new StudentRegistrationFormPage();
     @Test
     void successfulRegistrationFormTest() {
-        open("/automation-practice-form");
-        executeJavaScript("""
-        document.getElementById('fixedban')?.remove();
-        document.querySelector('footer')?.remove();
-        """);
-        $("#firstName").setValue(firstName);
-        $("#lastName").setValue(lastName);
-        $("#userEmail").setValue(userEmail);
-        $("#genterWrapper").$(byText(sex)).click();
-        $("#userNumber").setValue(userNumber);
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").$(byText(month)).click();
-        $(".react-datepicker__year-select").$(byText(year)).click();
-        $(".react-datepicker__day.react-datepicker__day--0" + day).click();
-        $("#subjectsInput").setValue(subject).pressEnter();
-        $("#hobbies-checkbox-3").click();
-        $("#uploadPicture").uploadFromClasspath(picture);
-        $("#currentAddress").setValue(currentAddress);
-        $("#state").click();
-        $(byText(state)).click();
-        $("#city").click();
-        $(byText(city)).click();
-        $("#submit").click();
+       studentRegistrationFormPage
+                .openPage()
+                .typeFirstName(firstName)
+                .typeLastName(lastName)
+                .typeUserEmail(userEmail)
+                .setGender(sex)
+                .typeUserNumber(userNumber)
+                .setDateOfBirth (day,month,year)
+                .setSubjects(subject)
+                .setHobby()
+                .uploadPicture(picture)
+                .setCurrentAddress(currentAddress)
+                .chooseStateAndCity(state,city)
+               .submitForm()
+               .resultWindowAppear()
+               .checkForm("Student Name",firstName + " " + lastName)
+               .checkForm("Student Email", userEmail)
+               .checkForm("Gender", sex)
+               .checkForm("Mobile", userNumber)
+               .checkForm("Date of Birth", day+" "+month+","+year)
+               .checkForm("Subjects", subject)
+               .checkForm("Hobbies", hobby)
+               .checkForm("Address", currentAddress)
+               .checkForm("Picture", picture)
+               .checkForm("State and City", state+" "+city)
+               .closeForm();
 
-        $(".modal-open").should(appear);
-        $(".table-responsive") .shouldHave(text(firstName));
-        $(".table-responsive") .shouldHave(text(lastName));
-        $(".table-responsive").shouldHave(text(userEmail));
-        $(".table-responsive").shouldHave(text(sex));
-        $(".table-responsive").shouldHave(text(userNumber));
-        $(".table-responsive").shouldHave(text(day + " " + month + "," + year));
-        $(".table-responsive").shouldHave(text(subject));
-        $(".table-responsive").shouldHave(text(hobby));
-        $(".table-responsive").shouldHave(text(currentAddress));
-        $(".table-responsive").shouldHave(text(picture));
-        $(".table-responsive").shouldHave(text(state + " " + city));
-        $("#closeLargeModal").click();
+
+
+
+
 
     }
 }
