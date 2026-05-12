97a78d0111ac3a5220328f65f549fcc6b072531b
cherry-yvnv <yvnvgg@gmail.com>
2026-05-12 19:18:40 +0300

    added AfterEach in TestBase

--- src/test/java/test_data/TestBase.java
+++ src/test/java/test_data/TestBase.java
@@ -1,6 +1,8 @@
 package test_data;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -8,5 +10,10 @@ public class TestBase {
     static void setupSelenideConfig() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @AfterEach
+    void afterEach() {
+        Selenide.closeWebDriver();
     }
 }
